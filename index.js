95aa3cd3cf5fa6c0fb8b22bb4efc9292d4d1bf30
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-06-12 11:24:55 +0430

    initial files and folders

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {Provider as StoreProvider} from 'react-redux';
+import {Provider as PaperProvider} from 'react-native-paper';
+
+import App from './app/App';
+import {store} from './app/store';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const root = () => {
+  <StoreProvider store={store}>
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  </StoreProvider>;
+};
+
+AppRegistry.registerComponent(appName, () => root);
