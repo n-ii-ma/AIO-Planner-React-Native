e2fbb1ea7fc796c9879b2fd2704709e70266da4e
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-06-13 13:13:15 +0430

    Redux-Persist

--- app/store.js
+++ app/store.js
@@ -1,15 +1,42 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import remainingTasksSliceReducer from '../features/remainingTasks/remainingTasksSlice';
 import tasksSliceReducer from '../features/task/tasksSlice';
 import transactionsSliceReducer from '../features/transaction/transactionsSlice';
 import weatherSliceReducer from '../features/weather/weatherSlice';
 
+const rootReducer = combineReducers({
+  transactions: transactionsSliceReducer,
+  remainingTasks: remainingTasksSliceReducer,
+  tasks: tasksSliceReducer,
+  weather: weatherSliceReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+  blacklist: ['weather'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    transactions: transactionsSliceReducer,
-    remainingTasks: remainingTasksSliceReducer,
-    tasks: tasksSliceReducer,
-    weather: weatherSliceReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
--- index.js
+++ index.js
@@ -6,16 +6,22 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import {Provider as StoreProvider} from 'react-redux';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import App from './app/App';
 import {store} from './app/store';
 import {name as appName} from './app.json';
 
+const persistor = persistStore(store);
+
 const root = () => (
   <StoreProvider store={store}>
-    <PaperProvider>
-      <App />
-    </PaperProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </PersistGate>
   </StoreProvider>
 );
 
